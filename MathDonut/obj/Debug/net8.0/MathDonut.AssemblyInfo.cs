c68b5cc7681c4156d61c0b73d3c670aa467088f6
GeneralGak <57255134+GeneralGak@users.noreply.github.com>
2026-05-26 19:33:41 +0200

    test

--- MathDonut/obj/Debug/net8.0/MathDonut.AssemblyInfo.cs
+++ MathDonut/obj/Debug/net8.0/MathDonut.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathDonut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01b4740d8f2c19b5bba35b057b1609af09bc75e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathDonut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathDonut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
